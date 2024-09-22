25b0bbbc51a3b9bad596c4e906bab22c1c7939b2
fqathf <100032449+fqathf@users.noreply.github.com>
2024-09-22 21:08:27 +0700

    Landing Page Only

--- proses_subject.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $data = array(
-        "subject" => $_POST["subject"],
-        "day" => $_POST["day"],
-        "times" => $_POST["times"]
-    );
-}
-?>
